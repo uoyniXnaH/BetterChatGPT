4bf18c0ab3eb5a20d5f1a5d447989a3e28533327
uoyniXnaH <b4baker@gmail.com>
2024-08-05 22:54:22 +0900

    change default model to 4omini

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -37,7 +37,7 @@ export const modelOptions: ModelOptions[] = [
   // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = 'gpt-3.5-turbo';
+export const defaultModel = 'gpt-4o-mini';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
